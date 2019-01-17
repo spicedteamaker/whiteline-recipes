082e2788b08e44dd0302f05faddc1cc6e89df054
teabrewer <teabrewer@teabrewer.com>
2019-01-17 16:13:07 -0500

    Add picture to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -71,7 +71,7 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.fetch(:recipe, {}).permit(:title, :ingredients, :instructions)
+      params.fetch(:recipe, {}).permit(:title, :ingredients, :instructions, :picture)
     end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,5 @@ class Recipe < ApplicationRecord
   has_many :instructions, dependent: :destroy
   validates :title, presence: true
   validates :ingredients, presence: true
+  has_one_attached :picture
 end
