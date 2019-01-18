cd346a5ac3534b5c8cbc319638e826d66ce4895d
teabrewer <teabrewer@teabrewer.com>
2019-01-18 14:13:32 -0500

    Adjust navbar items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :recipes do
     resources :instructions
   end
-  devise_for :users, skip: [:registrations]
+  devise_for :users
   as :user do
   get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
   #get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
